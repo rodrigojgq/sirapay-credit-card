6f82589246c1f5698b7cee6cbf6161754ca8ee39
rodrigojgq <rodrigojgq@gmail.com>
2021-04-21 17:35:03 -0400

    Test completados y lógica en validator terminada.

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,35 @@
 const validator = {
-  // ...
+  isValid(cardNumber) {
+    let valid = true;
+    let numbers = Number(cardNumber);
+    let i = 1;
+    let sum = 0;
+    while(numbers > 0) {
+      let aux = numbers % 10;
+      if(i % 2 === 0) {
+        aux = aux * 2;
+        if(aux >= 10){
+          aux = (aux % 10) + Math.trunc(aux / 10);
+        }
+      }
+      sum = sum + aux;
+      i++;
+      numbers = Math.trunc(numbers / 10);
+    }
+    valid = (sum % 10 == 0);
+    return valid;
+  },
+  maskify(cardNumber) {
+    let maskifiedNumber;
+    let numberLength = cardNumber.length;
+    maskifiedNumber = cardNumber;
+    if(numberLength > 4){
+      let aux = cardNumber.substring(numberLength-4)
+      maskifiedNumber = aux.padStart(numberLength, "#");
+    }
+    return maskifiedNumber;
+  }
 };
 
+validator.maskify("helloworld");
 export default validator;
