1aabecd59ea972c03eee32ce3ac825d718c46b30
rodrigojgq <rodrigojgq@gmail.com>
2021-04-29 17:22:04 -0400

    Updates

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const submitButton = document.querySelector("#submit-button");
 let clothesList = [];
+let oneChecked = true;
 
 submitButton.addEventListener("click", () => {
   const checkboxes = document.querySelectorAll('input[name="clothe"]:checked');
-  checkboxes.forEach(checkbox => {
-    let clotheInfo = [];
-    clotheInfo.push(checkbox.value);
-    clotheInfo.push(document.querySelector(`#${checkbox.value}-cost`).innerHTML);
-    clotheInfo.push(checkbox.getAttribute('id'));
-    clothesList.push(clotheInfo);
-  });
-  localStorage.setItem('clothes', JSON.stringify(clothesList))
+  if(checkboxes.length > 0){
+    checkboxes.forEach(checkbox => {
+      let clotheInfo = [];
+      clotheInfo.push(checkbox.value);
+      clotheInfo.push(document.querySelector(`#${checkbox.value}-cost`).innerHTML);
+      clotheInfo.push(checkbox.getAttribute('id'));
+      clothesList.push(clotheInfo);
+    });
+    localStorage.setItem('clothes', JSON.stringify(clothesList))
+    oneChecked = false;
+  }
+});
+
+const element = document.querySelector('form');
+element.addEventListener('submit', event => {
+  if(oneChecked){
+    event.preventDefault();
+    alert("Debe seleccionar al menos una opción");
+  }
 });
 
 function checkClothes() {
